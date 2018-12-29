e557ee3449045531a79cea73da7fb9ec20077573
BhavKaran <bhav.chahal97@gmail.com>
2018-12-29 20:46:10 +0530

    Update project.py

--- project.py
+++ project.py
@@ -2,19 +2,19 @@ import requests
 import bs4
 import os
 
-os.system('pip install requests bs4 lxml')  # install all dependencies
+# os.system('pip install requests bs4 lxml')  # install all dependencies
 
 res = requests.get('https://blog.counter-strike.net/index.php/category/updates/')
 soup = bs4.BeautifulSoup(res.text, 'lxml')
 post = soup.select('div.inner_post')
 update = post[0]
-info = update.getText()
-print (info)
 
 fout = open("output.html", "w")  # creates new file named output.html
 
 fout.write("<html><body>")  # html tags before data
-fout.write("%s" % info)  # data
+fout.write("%s" % update)  # data
 fout.write("</html></body>")  # html tags after data
 
 fout.close()  # close the function
+
+print ("\nExport Successful > project.html")
