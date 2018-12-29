0e95072dc42f296b8cdeedc0a1da31cd91ef086e
Avi Thour <thouravi@gmail.com>
2018-12-29 22:43:07 +0530

    Removed Headers, Footers and External Links

--- project.py
+++ project.py
@@ -10,35 +10,37 @@ soup = bs4.BeautifulSoup(res.text, "lxml")
 post = soup.select("div.inner_post")
 update = post[0]
 
-# creates new file named output.html and read all lines
-
 fout = open("output.html", "w")
-fout.write("%s" % update)  # data
+fout.write("%s" % update)
 fout.close()
 
 readFile = open("output.html")
 lines = readFile.readlines()
 readFile.close()
 
-# remove last lines from output.html file
-
 w = open("output.html", "w")
 w.writelines([item for item in lines[:-5]])
 w.close()
 
-# copy contents of output.html file to string
+with open("output.html", "r") as myfile:
+    data = myfile.read()
+
+readFile = open("output.html")
+lines = readFile.readlines()
+readFile.close()
+
+w = open("output.html", "w")
+w.writelines([item for item in lines[3:]])
+w.close()
 
 with open("output.html", "r") as myfile:
-    data = myfile.read().replace("\n", "")
+    data = myfile.read()
+    
+fout = open("output.html", "w")
+fout.write("<html><body>")
+fout.write("%s" % data)
+fout.write("</html></body>")
 
-# add html tags to output.html file
-
-fout = open("output.html", "w")  # creates new file named final_output.html
-
-fout.write("<html><body>")  # html tags before data
-fout.write("%s" % data)  # data
-fout.write("</html></body>")  # html tags after data
-
-fout.close()  # close the function
+fout.close()
 
 print("\nExport Successful > output.html")
